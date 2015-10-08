b1cff612872c330109aad11b38d10ad45d53eb81
marciomarinho <marciomarinho@gmail.com>
2015-10-08 21:15:04 +1100

    Adding Coordinate class

---
+++ src/main/java/losango/domain/Coordinate.java
@@ -0,0 +1,28 @@
+package losango.domain;
+
+/**
+ * Created by marciomarinho on 8/10/15.
+ */
+public class Coordinate {
+
+    private long latitude;
+    private long longitude;
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+
+}
